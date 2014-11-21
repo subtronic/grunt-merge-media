ccdf86a1509f2cf30fc38813aea3af0f2a5b7818
Mike Cook <mike.cook@fanduel.com>
2014-11-21 17:18:25 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-grunt-merge-media
-=================
+# grunt-merge-media
 
-Grunt plugin to combine duplicate media queries
+> Merge duplicate media queries
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-merge-media --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-merge-media');
+```
+
+## The "merge_media" task
+
+### Overview
+In your project's Gruntfile, add a section named `merge_media` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  merge_media: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+});
+```
+
+### Options
+
+#### options.compress
+Type: `Boolean`
+Default value: `false`
+
+A Boolean value that will switch the compression of the processed file. This is passed straight into the `stringify` method in [css](https://github.com/reworkcss/css).
+
+### Usage Examples
+
+```js
+grunt.initConfig({
+  merge_media: {
+    options: {
+      compress: true
+    },
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  }
+});
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/merge_media.js
@@ -0,0 +1,88 @@
+/*
+ * grunt-merge-media
+ * https://github.com/migstopheles/grunt-merge-media
+ *
+ * Copyright (c) 2014 Mike Cook
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('merge_media', 'Merge duplicate media queries', function() {
+    // Load dependencies
+    var css = require('css');
+    var path = require('path');
+    var _ = require('lodash');
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      compress: false
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(file) {
+      var src = file.src.filter(function(filePath) {
+        if (!grunt.file.exists(filePath)) {
+          grunt.log.warn('Source file "' + filePath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).forEach(function (filePath) {
+
+        var fileName = filePath.replace(/(.*)\//gi, '');
+        var fileExt = '.' + fileName.replace(/(.*)\./gi, '');
+        var fileDest = file.dest;
+        var output = '';
+
+        grunt.log.writeln(fileExt);
+
+        if (fileDest.indexOf(fileExt) === -1) {
+          fileDest = path.join(fileDest, fileName);
+        }
+
+        var source = grunt.file.read(filePath);
+        var parsedCss = css.parse(source);
+
+        var queries = _.filter(parsedCss.stylesheet.rules, { type: 'media' });
+        var rules = _.difference(parsedCss.stylesheet.rules, queries);
+        var grouped = _.groupBy(queries, function(rule) { return rule.media; });
+
+        var merged = _.map(grouped, function(group) {
+          return {
+            type: group[0].type,
+            media: group[0].media,
+            rules: _.flatten(_.pluck(group, 'rules'), true)
+          };
+        });
+
+        output += css.stringify({
+          stylesheet: {
+            rules: rules
+          }
+        }, options);
+
+        output += '\n';
+
+        output += css.stringify({
+          stylesheet: {
+            rules: merged
+          }
+        }, options);
+
+        output += '\n';
+
+        grunt.file.write(fileDest, output);
+
+        grunt.log.writeln('Cleaned media queries for ' + fileName);
+
+      });
+    });
+  });
+
+};
